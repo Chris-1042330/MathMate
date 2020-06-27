57ae16a5c70b9b807aada132b891834ef3900487
samsorwiar <99462@mborijnland.nl>
2020-06-27 19:15:05 +0200

    Update Presentatie_pagina.php
    added bootstrap

--- Presentatie_pagina.php
+++ Presentatie_pagina.php
@@ -1,57 +1,59 @@
-<!DOCTYPE html>
+<!doctype html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="language" constent="NL">
-	<meta name="description" constent="events">
-	<meta name="author" constent="Samsor Wiar">
-	<meta name="keywords" constent="presenteren | Presentatie-Pagina">
-	<title>Presentatie-Pagina</title>
-	<!-- standaard  -->
-	 <script src="js/slideshow.js"></script> 
-	<link rel="stylesheet" type="text/css" href="css/standard.css">
-	<link rel="stylesheet" type="text/css" href="css/navbar.css">
-	<!-- optionieel -->
-	<link rel="stylesheet" type="text/css" href="css/Presentatie.css">
-</head>
-<body>
-	<main class="container">
-		<header>
-			<nav class="nav">
-				<div class='nav_standard'>
-					<span>
-						<!-- <a class="login" href="login.php">Log In</a>
-						<a class="login" href="login.php">Log out</a> -->
-					</span>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="language" constent="NL">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" constent="">
+		<meta name="author" constent="">
+		<meta name="keywords" constent="">
+		<title>Presentatie-Pagina</title>
+		<!-- koppelen van bootstrap -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+		 <!-- koppelen van overige style  -->
+		<script src="js/slideshow.js"></script> 
+		<link rel="stylesheet" type="text/css" href="css/#.css">
+		<!-- optionieel -->
+		<link rel="stylesheet" type="text/css" href="css/present.css">
+	<head>
+	
+	<body class="container col-md-12 bg-light">
+		<header class="row">	
+			<nav class="navbar navbar-light bg-light  col-md-12">
+			  <a class="logo" href=""><img class="align-items-center logo" src="img/logo.png"></a>
+			  <div class="row">
+			  	<div class="login">
+				 	<a class="col-md-12" href="login.php">Login</a>
 				</div>
-				<span class='nav_img'>
-					<img src="img/logo.png" alt="MathMate">
-				</span>
-				<div class='nav_extra'>
-					<span>
-						<a class="login" href="login.php">Log In</a>
-					</span>
+				<div class="register">
+					 <a class="col-md-12" href="register.php">Registreren</a>
 				</div>
+			</div>
+			  <hr class="line">
 			</nav>
 		</header>
 
-		<section class="homepage">
-			<header>
-				<h1 class="welkom_title"> Welkom Bij MathMate!</h1>
-			</header>
+       <main class=" col-md-12 d-sm-block col-sm-5">
+			<h1 class="col-md-12 mt-5 text-center welkom_title">Welkom Bij MathMate!</h1>
+				
+			<section class="row col-md-12">
+				<article class="col-md-6 d-flex p-2 bd-highlight">
+					<p class="row col-md-10 col-sm-5 mx-5 mt-5">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <br><br> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+				</article>
 
-			<article class="welkom_content">
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <br><br> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-				</p>
-			</article>
-
-			<div class="slideshow">
-				<div onload='changeImg()' >
-				    <img name='slide' src="https://www.cssdesignawards.com/articleimages/20131231/25-web-place.jpg" height="350px" />
+				<div class="col-md-5 mx-5 mt-5 slideshow ">
+					<div onload='changeImg()' >
+					    <img name='slide' src="https://www.cssdesignawards.com/articleimages/20131231/25-web-place.jpg" height="350px" />
+					</div>
 				</div>
-			</div>
-		</section> 
-	</main>
-</body>
+			</section> 
+		</main> 
+		<!--  bootstrap bestanden  -->
+		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+	</body>
 </html>
+	
