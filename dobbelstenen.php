694e8329acb6d998aa22c249c6f37d2faec625b0
samsorwiar <99462@mborijnland.nl>
2020-06-29 16:58:17 +0200

    Update dobbelstenen.php

--- dobbelstenen.php
+++ dobbelstenen.php
@@ -1,5 +1,4 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
 <?php
 session_start();
 if(!isset($_SESSION['ingelogd'])){
@@ -7,37 +6,57 @@ if(!isset($_SESSION['ingelogd'])){
 }
 require('header_loggedin.php');
 ?>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="language" constent="NL">
-    <meta name="description" constent="events">
-    <meta name="author" constent="Samsor Wiar">
-    <meta name="keywords" constent="Dobbelstenen gooien">
-    <title>Rad van fortuin</title>
-    <link rel="stylesheet" type="text/css" href="css/dobbelstenen.css">
-    <link rel="stylesheet" type="text/css" href="css/standard.css">
-    <link rel="stylesheet" type="text/css" href="css/navbar.css">
-    <script src='js/raderen.js'></script>
-</head>
-<body>
-    <main class="container-fluid">
-        <button type="button" onclick='draaiRad()'>Draai</button><br>
-        <button type="button" onclick='reset()'>reset</button>
-            <div>
-                <p id="waarde">0</p>
-            <div>
-                <div class="rad">
-                    <img src="img/dRad.png">
-                </div>
-                <img class="pijl" src="img/pijl.png">
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="language" constent="NL">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" constent="">
+        <meta name="author" constent="">
+        <meta name="keywords" constent="">
+        <title>Dobbel</title>
+        <!-- koppelen van bootstrap -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <!-- koppelen van overige style -->
+        <link rel="stylesheet" type="text/css" href="css/dobbelstenen.css">
+        <script src='js/raderen.js'></script>
+    </head>
+
+    <body class="container">
+        <main class="row col-md-12 d-sm-block col-sm-12 mt-5">
+            <div class="d-block">
+                <button type="button" onclick='draaiRad()'>Draai</button><br>
+                <button type="button" onclick='reset()'>reset</button>
             </div>
-            <div>
-                <div class="rad">
-                    <img src="img/dRad.png">
+
+            <div class="row col-md-12 m-auto">
+                        <p id="waarde">0</p>
+                <div class="row mt-5 ">
+                    <div class="col-md-12 col-6 rad">
+
+                        <div class="rad">
+                            <img class="pijl" src="img/pijl.png">
+                            <img class="radFoto" src="img/dRad.png">
+                        </div>
+                    </div>
                 </div>
-                <img class="pijl" src="img/pijl.png">
+                <div class="row mt-5">
+                    <div class="col-md-12 col-6 rad">
+
+                        <div class="rad">
+                            <img class="pijl" src="img/pijl.png">
+                            <img class="radFoto" src="img/dRad.png">
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-    </main>
-    
-<?php require('footer.php');?>
+        </main>
+            
+        <?php require('footer.php');?>
+
+        <!-- bootstrap bestanden -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    </body>
+</html>
