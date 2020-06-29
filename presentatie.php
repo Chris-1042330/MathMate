22e85c5a7a12700abbc2f4752e1fb69a3e6d5900
samsorwiar <99462@mborijnland.nl>
2020-06-29 14:24:20 +0200

    Update presentatie.php

--- presentatie.php
+++ presentatie.php
@@ -26,13 +26,25 @@ if(!isset($_SESSION['ingelogd'])){
 					</p>
 				</article>
 
-				<div class="col-12  mt-5  text-center">
-					<div class="slideshow " onload='changeImg()' >
-					    <img name='slide'  src="https://www.cssdesignawards.com/articleimages/20131231/25-web-place.jpg"/>
+				<div class="carousel slide col-sm-12 col-md-12 col-lg-8 mx-auto slideshow" data-ride="carousel">
+						<div class="carousel-inner " role="listbox">
+							<div class="carousel-item active mt-5">
+								<div class="row">
+									<img class="col d-block w-100 " src='https://www.cssdesignawards.com/articleimages/20131231/25-web-place.jpg'>
+								    <img class="col d-block w-100 " src='https://cdn.dribbble.com/users/1118096/screenshots/3450211/dribbble_shot_4.jpg'>
+								    <img class="col d-block w-100 " src='https://i.pinimg.com/originals/08/fd/8f/08fd8f8278d71e4133162215f6436490.jpg'>
+								</div>
+							</div>
+							<div class="carousel-item mt-5 ">
+								<div class="row">
+									<img class="col d-block w-100 " src='https://cdn.dribbble.com/users/906563/screenshots/5150522/cover.jpg';>
+								    <img class="col d-block w-100 " src="https://i.pinimg.com/236x/83/7b/a3/837ba330f9cb65c151e0da9e410a9d6f--contact-page-contact-form.jpg">
+								    <img class="col d-block w-100 " src="https://images.ui8.net/uploads/1350x1020-1_1555933560065.jpg">
+								</div>
+							</div>
+						</div>
 					</div>
-				</div>
-			</div>
-		</section> 
-	</main>
+			</section> 
+		</main>
 	
 <?php require('footer.php');?>
